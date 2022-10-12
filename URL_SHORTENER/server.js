84446856872880243d6b36f009c03653cc64441c
rakesh <rnarasimhaiah@livevox.com>
2022-10-12 19:19:08 +0530

    small changes

--- URL_SHORTENER/server.js
+++ URL_SHORTENER/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+app.set('view engine','ejs');
 
+
+
+app.use('/',(req,res)=> {
+    res.render('index')
+})
 
 app.listen(process.env.PORT || 5000)
